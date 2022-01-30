1f7eb006ee3919b2aa99e7d636783bc016030b2b
Stevenijames <ijamesproductions1@gmail.com>
2022-01-29 21:17:34 -0700

    config for backend connect to mongodb

--- backend1/server.js
+++ backend1/server.js
@@ -1,12 +1,16 @@
-const express = require("express");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import products from "./data/products.js";
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
-const port = process.env.PORT || 5000;
-
 app.get("/", (req, res) => {
-  res.send("API is running...");
+  res.send("API is running....");
 });
 
 app.get("/api/products", (req, res) => {
@@ -18,4 +22,8 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
